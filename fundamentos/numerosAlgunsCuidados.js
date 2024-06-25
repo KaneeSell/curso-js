75fb53b04a35f73d5d8a26786404c793813acc15
Daniel Henrique Sell <kanee.sell@gmail.com>
2024-06-25 06:52:45 -0300

    first

---
+++ fundamentos/numerosAlgunsCuidados.js
@@ -0,0 +1,11 @@
+console.log(7 / 0); //Infinity
+
+console.log("10" / 2); //Cuidar com operações matematicas usando string
+
+console.log("Show" * 2); //Não suporta string para calculos, gera not a number
+
+console.log(0.1 + 0.7); //Pode ter impressoção
+
+// console.log(10.toString()); Não consolse usando literal chamar a função
+
+console.log((10.345).toFixed(2)); //consegueme chamar a função usando literal se conter parentes
---
+++ fundamentos/variaveisEConstantes.js
@@ -0,0 +1,15 @@
+var a = 3;
+let b = 4;
+
+var a  = 30; // podemos declarar var 2 ou mais verzes
+// let b = 40; Não podemos declarar let 2 ou mais vezes
+
+console.log(a, b);
+
+a = 300;
+b = 400;
+console.log(a, b);
+
+const c = 5;
+// c = 10; Não podemos alterar o valor de uma constante
+console.log(c);
