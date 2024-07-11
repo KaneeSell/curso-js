b401246c1fcc6b4998be77e510e42c197fe5b3e1
Daniel Sell <103097217+KaneeSell@users.noreply.github.com>
2024-07-11 13:33:02 -0300

    second exercicios

---
+++ capitulo-2/Exercicio/JavaScript1.js
@@ -0,0 +1,7 @@
+function calcular(num1, num2) {
+    console.log('Soma =', num1 + num2)    
+    console.log('Subtração =', num1 - num2)    
+    console.log('Multiplicação =', num1 * num2)    
+    console.log('Divisão =', num1 / num2)    
+}
+calcular(5, 5)
---
+++ capitulo-2/Exercicio/JavaScript2.js
@@ -0,0 +1,9 @@
+function triangulo(lado1, lado2, lado3) {
+    if(lado1 == lado2) && (lado2 == lado3) {
+        return: 'Equilátero'
+    } else if(lado1 == lado2) || (lado2 == lado3) || (lado3 == lado1) {
+
+    } else {
+
+    }
+}
