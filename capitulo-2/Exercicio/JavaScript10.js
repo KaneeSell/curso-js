372b5496eec1f1855347a3a065ee2305eeddae10
Daniel Henrique Sell <kanee.sell@gmail.com>
2024-07-16 13:31:24 -0300

    first

--- capitulo-2/Exercicio/JavaScript10.js
+++ capitulo-2/Exercicio/JavaScript10.js
@@ -1,11 +1,7 @@
-const div3 = () => {
-    let resto = num % 3   
-    if(num != 0 && num % 3 === 0) {
-        return true
-    }
-    return false
+const div3 = (num) => {  
+    return num != 0 && num % 3 === 0;
 }
 for(let i = 0; i < 101; i++){
-    num = i
-    console.log(`${num} é divisivel por 3? R:${div3()}`)
+    let num = i
+    console.log(`${num} é divisivel por 3? R:${div3(num)}`)
 }
