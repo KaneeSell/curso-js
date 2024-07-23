89642d0c1bb90522583272507082234571fbbea6
Daniel Henrique Sell <kanee.sell@gmail.com>
2024-07-23 06:56:46 -0300

    first

--- capitulo-2/Exercicio/JavaScript37.js
+++ capitulo-2/Exercicio/JavaScript37.js
@@ -1,6 +1,14 @@
 function pA(n, a1, r){
     let termosDaPA = []; let somaTermosPA = 0
+    termodaPA = [a1, a1 + r, a1 + 2 * r, a1 + 3 * r];
+    somaTermosPA = a1 + (n - 1) * r;
+    console.log(`temos da Progressão Aritmética = [${termodaPA},...]\nSoma dos termos = ${somaTermosPA}`)
 }
-function pG(n, a1, r){
+function pG(n, a1, q){
     let termosDaPG = []; let somaTermosPG = 0
+    termosDaPG = [a1, a1 + 1 * q, a1 + 3 * q];
+    somaTermosPG = a1 * q ** (n - 1);
+    console.log(`temos da Progressão Aritmética = [${termosDaPG},...]\nSoma dos termos = ${somaTermosPG}`)
 }
+pA(10, 26, 5)
+pG(20, 2, 2)
---
+++ capitulo-2/Exercicio/JavaScript38.js
@@ -0,0 +1,5 @@
+function contador(inicio, fim){
+    if(inicio>fim&&inicio>=0&&fim<=100){for(let i=0;)}
+    else if(fim > inicio&&fim>=0&&inicio>=100){}
+    else{console.log('Inicio e fim inválidos!')}
+}
